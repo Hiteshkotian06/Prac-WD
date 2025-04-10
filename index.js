d5fa296825ad2d5dcf8d7bc793d3b2ca2662db92
Hiteshkotian06 <126050661+Hiteshkotian06@users.noreply.github.com>
2025-04-11 00:08:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hii")
+console.log("THis is the just few start of js")
+
+window.alert("My first alert")
+window.alert("My second alert")
+
+werrrwtrrtertertetriefjsfjlaksjfdklasjdfjsfaskldfjjisjijaasfjaskdfajientinasnt34r4rkwfnkasn
