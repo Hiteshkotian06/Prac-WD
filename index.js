7cac7535aae7d6ea335ed63bb7107d1aea7dea90
Hiteshkotian06 <126050661+Hiteshkotian06@users.noreply.github.com>
2025-04-12 09:45:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ console.log("THis is the just few start of js")
 window.alert("My first alert")
 window.alert("My second alert")
 
+alert("Just seeing if only alert give the alert or not")
 werrrwtrrtertertetriefjsfjlaksjfdklasjdfjsfaskldfjjisjijaasfjaskdfajientinasnt34r4rkwfnkasn
