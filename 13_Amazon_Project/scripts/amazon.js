eaa2723e81c30bf8bab59970e8bc2b9360b0ae7e
Amazon Project <hitesh.kotian06@gmail.com>
2025-09-11 02:40:59 +0530

    Directly Used product.js for product Data

--- 13_Amazon_Project/scripts/amazon.js
+++ 13_Amazon_Project/scripts/amazon.js
@@ -1,21 +1,23 @@
-const products = [{
-  image : "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name : "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating : {stars : 4.5, count : 87},
-  priceCents : 1090    //Here we used cent and not the decimal bcoz javascript create issue while working withe decimal
-},
-{
-  image : "images/products/intermediate-composite-basketball.jpg",
-  name : "Intermediate Size Basketball",
-  rating : {stars : 4, count : 127},
-  priceCents : 2095    //Here we used cent and not the decimal bcoz javascript create issue while working withe decimal
-},
-{
-  image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name : "Adults Plain Cotton T-Shirt - 2 Pack",
-  rating : {stars : 4.5, count : 56},
-  priceCents : 799     //Here we used cent and not the decimal bcoz javascript create issue while working withe decimal
-}]
+// const products = [{
+//   image : "images/products/athletic-cotton-socks-6-pairs.jpg",
+//   name : "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//   rating : {stars : 4.5, count : 87},
+//   priceCents : 1090    //Here we used cent and not the decimal bcoz javascript create issue while working withe decimal
+// },
+// {
+//   image : "images/products/intermediate-composite-basketball.jpg",
+//   name : "Intermediate Size Basketball",
+//   rating : {stars : 4, count : 127},
+//   priceCents : 2095    //Here we used cent and not the decimal bcoz javascript create issue while working withe decimal
+// },
+// {
+//   image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//   name : "Adults Plain Cotton T-Shirt - 2 Pack",
+//   rating : {stars : 4.5, count : 56},
+//   priceCents : 799     //Here we used cent and not the decimal bcoz javascript create issue while working withe decimal
+// }]
+
+// We will be using the product data directly from the product file in Data file -- /13_Amazon_Project/data/products.js
 
 let productHtml = ''
 
