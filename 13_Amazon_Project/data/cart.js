03a37be7bf4e66560e66951e8499502ea5dcde93
Amazon Project <hitesh.kotian06@gmail.com>
2025-09-16 02:08:33 +0530

    Save Cart in localStorage and also header Amazon is linked to my own file

--- 13_Amazon_Project/data/cart.js
+++ 13_Amazon_Project/data/cart.js
@@ -1,10 +1,27 @@
-export let cart = [{
+export let cart;
+
+try {
+  cart = JSON.parse(localStorage.getItem('cart'));
+  if (!Array.isArray(cart)) {
+    cart = null; // valid JSON, but not an array -- for emtpy json have only {} which throw error thus use - try catch
+  }
+} catch (e) {
+  cart = null; // if JSON.parse fails
+}
+
+if(!cart ) {
+  cart =   [{
   prod_id : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity : 2
 }, {
   prod_id : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity : 1
 }];
+}
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // Add to Cart funtion
 // We had this in the amazon.js we shifted this to this file to make it more understanding same for updateCartQuantity
@@ -24,9 +41,10 @@ export function addToCart(prod_id, selectedQuantity){
       quantity : selectedQuantity
     });
     } 
+    saveToStorage();
 }
 
-// Cart Update quantity function
+// Cart Update quantity function - in amazon to show number
 export function updateCartQuantity(){
   let cartQuantity = 0;
     cart.forEach((cartItem) => {
@@ -47,4 +65,6 @@ export function removeFromCart(productId){
   // cart = newCart;
 
   cart = cart.filter((cartItem => cartItem.prod_id !== productId))  //-- this work same as above also short and clean
+
+  saveToStorage();
 }
